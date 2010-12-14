1646aae6b5990123dbdfcedba33c3b8ef0dfb87a
Apocalypse <perl@0ne.us>
2010-12-13 20:20:05 -0700

    convert rest of module for dzil

--- lib/Dist/Zilla/Plugin/DualBuilders.pm
+++ lib/Dist/Zilla/Plugin/DualBuilders.pm
@@ -1,14 +1,19 @@
 package Dist::Zilla::Plugin::DualBuilders;
-use strict; use warnings;
-our $VERSION = '0.03';
 
-use Moose 1.01;
+# ABSTRACT: Allows use of Module::Build and ExtUtils::MakeMaker in a dzil dist
 
-# TODO wait for improved Moose that allows "with 'Foo::Bar' => { -version => 1.23 };"
-use Dist::Zilla::Role::PrereqSource 3.101461;
-use Dist::Zilla::Role::InstallTool 3.101461;
-with 'Dist::Zilla::Role::PrereqSource';
-with 'Dist::Zilla::Role::InstallTool';
+use Moose 1.03;
+
+with 'Dist::Zilla::Role::PrereqSource' => { -version => '3.101461' };
+with 'Dist::Zilla::Role::InstallTool' => { -version => '3.101461' };
+
+=attr prefer
+
+Sets your preferred builder. This builder will be the one added to the prereqs. Valid options are: "make" or "build".
+
+The default is: build
+
+=cut
 
 {
 	use Moose::Util::TypeConstraints 1.01;
@@ -77,7 +82,7 @@ sub register_prereqs {
 
 			# As of DZIL v2.101170 DZ:P:Makemaker adds to configure only
 			$config_prereq->clear_requirement( 'ExtUtils::MakeMaker' );
-			$self->log_debug( 'Preferring "build", removing ExtUtils::MakeMaker from prereqs' );
+			$self->log_debug( 'Preferring Module::Build, removing ExtUtils::MakeMaker from prereqs' );
 		} elsif ( $self->prefer eq 'make' ) {
 			# Get rid of MB stuff
 			$self->_buildver( $config_hash->{'Module::Build'} );
@@ -85,7 +90,7 @@ sub register_prereqs {
 			# As of DZIL v2.101170 DZ:P:ModuleBuild adds to configure and build
 			$config_prereq->clear_requirement( 'Module::Build' );
 			$build_prereq->clear_requirement( 'Module::Build' );
-			$self->log_debug( 'Preferring "make", removing Module::Build from prereqs' );
+			$self->log_debug( 'Preferring ExtUtils::MakeMaker, removing Module::Build from prereqs' );
 		}
 	} elsif ( exists $config_hash->{'Module::Build'} and $self->prefer eq 'make' ) {
 		$self->log_fatal( 'Detected Module::Build in the config but you preferred ExtUtils::MakeMaker!' );
@@ -104,11 +109,9 @@ __PACKAGE__->meta->make_immutable;
 
 =pod
 
-=for stopwords AnnoCPAN CPAN CPANTS Kwalitee MakeMaker ModuleBuild RT dist dzil prereq prereqs
+=for stopwords MakeMaker ModuleBuild dist dzil prereq prereqs
 
-=head1 NAME
-
-Dist::Zilla::Plugin::DualBuilders - Allows use of Module::Build and ExtUtils::MakeMaker in a dzil dist
+=for Pod::Coverage register_prereqs setup_installer
 
 =head1 DESCRIPTION
 
@@ -121,87 +124,7 @@ and this is obviously not what you want!
 	[MakeMaker] ; or [MakeMaker::Awesome], will work too :)
 	[DualBuilders] ; needs to be specified *AFTER* the builders
 
-This plugin accepts the following options:
-
-=over 4
-
-=item * prefer
-
-Sets your preferred builder. This builder will be the one added to the prereqs. Valid options are: "make" or "build".
-
-The default is: build
-
-=back
-
 =head1 SEE ALSO
-
-L<Dist::Zilla>
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-	perldoc Dist::Zilla::Plugin::DualBuilders
-
-=head2 Websites
-
-=over 4
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Dist-Zilla-Plugin-DualBuilders>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Dist-Zilla-Plugin-DualBuilders>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Dist-Zilla-Plugin-DualBuilders>
-
-=item * CPAN Forum
-
-L<http://cpanforum.com/dist/Dist-Zilla-Plugin-DualBuilders>
-
-=item * RT: CPAN's Request Tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-Plugin-DualBuilders>
-
-=item * CPANTS Kwalitee
-
-L<http://cpants.perl.org/dist/overview/Dist-Zilla-Plugin-DualBuilders>
-
-=item * CPAN Testers Results
-
-L<http://cpantesters.org/distro/D/Dist-Zilla-Plugin-DualBuilders.html>
-
-=item * CPAN Testers Matrix
-
-L<http://matrix.cpantesters.org/?dist=Dist-Zilla-Plugin-DualBuilders>
-
-=item * Git Source Code Repository
-
-L<http://github.com/apocalypse/perl-dist-zilla-plugin-dualbuilders>
-
-=back
-
-=head2 Bugs
-
-Please report any bugs or feature requests to C<bug-dist-zilla-plugin-dualbuilders at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-Plugin-DualBuilders>.  I will be
-notified, and then you'll automatically be notified of progress on your bug as I make changes.
-
-=head1 AUTHOR
-
-Apocalypse E<lt>apocal@cpan.orgE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2010 by Apocalypse
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-The full text of the license can be found in the LICENSE file included with this module.
+Dist::Zilla
 
 =cut
